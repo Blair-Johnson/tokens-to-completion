635a4f876a18d8ca9ec3d07071eb54495822d4c1
Blair-Johnson <blairjohnson@gatech.edu>
2023-12-22 11:49:55 -0500

    GPT2 down to attention implementation. Need to modify causal mask

--- launch.sh
+++ launch.sh
@@ -4,6 +4,6 @@ python -i run_clm_no_trainer.py \
     --model_name_or_path gpt2 \
     --output_dir ./tmp/test-clm \
     --per_device_train_batch_size 32 \
-    --preprocessing_num_workers 64 \
+    --preprocessing_num_workers 48 \
     #--overwrite_cache \
 
--- test.py
+++ test.py
@@ -1,19 +1,41 @@
 import random
 from ttc import *
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from gpt2 import *
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
 from datasets import load_dataset
 
 
-model = AutoModelForCausalLM.from_pretrained("gpt2")
+#model = AutoModelForCausalLM.from_pretrained("gpt2")
+config = AutoConfig.from_pretrained("gpt2")
+model = GPT2TTC._from_config(config)
+
+
 tokenizer = AutoTokenizer.from_pretrained("gpt2")
 dataset = load_dataset("wikimedia/wikipedia", "20231101.en")
-tok_text = tokenizer(dataset['train'][0]['text'])
 
-ttc_tokens = create_ttc_tokens()
-add_ttc_tokens(model, tokenizer, ttc_tokens)
+label_stack = []
+input_id_stack = []
+ttc_id_stack = []
+attention_mask_stack = []
+for i in range(3):
+    sample = tokenizer(dataset['train'][i]['text'])
+    input_ids = sample['input_ids']
 
-ttc_numbers = index_tokens_log2(tok_text)
-samp = random.sample(ttc_numbers, 30)
+    ttc_tokens = create_ttc_tokens()
+    add_ttc_tokens(model, tokenizer, ttc_tokens)
 
-mapped = map_values_to_closest_labels(samp, ttc_tokens)
+    ttc = map_seq_to_ttc(input_ids, ttc_tokens)
+    ttc = tokenizer(''.join(ttc))['input_ids']
+    input_ids.append(tokenizer.eos_token_id)
 
+    label_stack.append(input_ids.copy()[-128:])
+    input_id_stack.append(input_ids.copy()[-128:])
+    ttc_id_stack.append(ttc[-128:])
+    attention_mask_stack.append(sample['attention_mask'][-128:])
+
+sample['labels'] = torch.tensor(label_stack)
+sample['input_ids'] = torch.tensor(input_id_stack)
+sample['ttc_ids'] = torch.tensor(ttc_id_stack)
+sample['attention_mask'] = torch.tensor(attention_mask_stack)
+
+res = model(**sample)
